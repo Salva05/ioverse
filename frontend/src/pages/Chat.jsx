0478d6debfa5b57c17ce4fcb5fa28c26a1edcfb6
Salva05 <salvatorelombardi.98@hotmail.it>
2024-10-12 22:32:31 +0200

    Defined basic backend structure. Added Django Rest Framework as dependency. Added Chatbot module. Created first app internal to Django 'Chatbot'. Yet to implement DRF structure. Yet to clean chatbot_modules from unneeded stuff.

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,28 +1,67 @@
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import { useState } from "react";
 import {
   MainContainer,
   ChatContainer,
   MessageList,
   Message,
   MessageInput,
+  TypingIndicator
 } from "@chatscope/chat-ui-kit-react";
-import React from "react";
 
 const Chat = () => {
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "How can I help you?",
+      sentTime: "just now",
+      sender: "Chatbot",
+      direction: "incoming"
+    },
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sentTime: "one minte ago",
+      sender: "user",
+      direction: "outgoing"
+    }
+
+    const newMessages = [...messages, newMessage]; // all the old messages + the new message
+
+    // update out messages state
+    setMessages(newMessages);
+
+    // set typing indicator
+    setTyping(true);
+
+    // process message to backend
+    await processMessageToBackend(newMessages);
+  }
+
+  async function processMessageToBackend(chatMessages) {
+
+  }
+
   return (
-    <div style={{ position: "relative", height: "100%", width: "100%", display: "flex" }}>
-      <MainContainer style={{ flex: 1, height: "100%" }}>
+    <div
+      style={{
+        position: "relative",
+        height: "100%",
+        width: "100%",
+        display: "flex",
+      }}
+    >
+      <MainContainer style={{ flex: 1 }}>
         <ChatContainer>
-          <MessageList>
-            <Message
-              model={{
-                message: "Hello my friend",
-                sentTime: "just now",
-                sender: "Joe",
-              }}
-            />
+          <MessageList
+            typingIndicator={typing ? <TypingIndicator content="Reasoning"/> : null}>
+            {messages.map((message, id) => (
+              <Message key={id} model={message} />
+            ))}
           </MessageList>
-          <MessageInput placeholder="Type message here" />
+          <MessageInput placeholder="Type message here" onSend={handleSend}/>
         </ChatContainer>
       </MainContainer>
     </div>
