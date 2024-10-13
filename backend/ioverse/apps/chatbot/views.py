e1e06ff666ad4374dfe23d93be54acf616d7427f
Salva05 <salvatorelombardi.98@hotmail.it>
2024-10-13 10:07:53 +0200

    Defined main API endpoint. Yet to decide which inheriting model is best suited for the task among DRF's ones

--- backend/ioverse/apps/chatbot/views.py
+++ backend/ioverse/apps/chatbot/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+
+class Chatbot(APIView):
+    """
+    Hanldes incoming POST requests, routing it's body 
+    into chatbot_modules and returns a message representing the reponse
+    
+    * Requires token authentication.
+    """
+    
+    # authentication_classes = [authentication.TokenAuthentication]
+    # permission_classes = [permissions.IsAdminUser]
+
+    def post(self, request, format=None):
+        pass
