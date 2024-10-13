bf17bc27e1084c02e3d317a71b83890f30795a0c
Salva05 <salvatorelombardi.98@hotmail.it>
2024-10-13 10:10:41 +0200

    Created first model 'Conversation' to store a user's set of chats.

--- backend/ioverse/apps/chatbot/models.py
+++ backend/ioverse/apps/chatbot/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Conversation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Conversation {self.di} with {self.user.username}"
