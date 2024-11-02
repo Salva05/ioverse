9934f3c538664b9bba889fa002acfdbee2c92bb3
Salva05 <salvatorelombardi.98@hotmail.it>
2024-11-02 12:12:53 +0100

    Moved User's url scope to root domain.

--- backend/ioverse/apps/text_to_image/urls.py
+++ backend/ioverse/apps/text_to_image/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ImageGenerationViewSet, SharedImageView, CurrentUserView
+from .views import ImageGenerationViewSet, SharedImageView
 
 router = DefaultRouter()
 router.register(r'image-generations', ImageGenerationViewSet, basename='image-generation')
@@ -8,5 +8,4 @@ router.register(r'image-generations', ImageGenerationViewSet, basename='image-ge
 urlpatterns = [
     path('', include(router.urls)),
     path('shared/<uuid:share_token>/', SharedImageView.as_view(), name='shared-image'),
-    path('api/current-user/', CurrentUserView.as_view(), name='current-user'),
 ]
--- backend/ioverse/ioverse/urls.py
+++ backend/ioverse/ioverse/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
-from rest_framework_simplejwt import views as jwt_views
-from apps.chatbot.views import UserRegistrationView
 from django.conf.urls.static import static
+
+from rest_framework_simplejwt import views as jwt_views
+
+from apps.chatbot.views import UserRegistrationView
+from apps.text_to_image.views import CurrentUserView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +31,5 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/current-user/', CurrentUserView.as_view(), name='current-user'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
