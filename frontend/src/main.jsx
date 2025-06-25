807bc50b40229452a05b1e6f811c10a144b2807b
Salva05 <salvatorelombardi.98@hotmail.it>
2025-06-25 16:59:39 +0200

    feat: wire root url path to /home as default

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SignUp from "./pages/Register";
@@ -24,6 +28,10 @@ const router = createBrowserRouter(
       path: "/",
       element: <Layout />,
       children: [
+        {
+          index: true, // matches the parent path “/”
+          element: <Navigate to="home" replace />,
+        },
         {
           path: "home",
           element: <Home />,
