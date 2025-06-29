24e6b1736e08384cb62a5c2ade96fdaa2a3a8d27
Salva05 <salvatorelombardi.98@hotmail.it>
2025-06-29 17:20:08 +0200

    feat: expose endpoint for admin key update

--- backend/ioverse/apps/account/urls.py
+++ backend/ioverse/apps/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PasswordResetRequestView
+from .views import PasswordResetRequestView, AdminKeySetView
 
 urlpatterns = [
     path('reset-password/', PasswordResetRequestView.as_view(), name='reset-password'),
+    path("admin-key/", AdminKeySetView.as_view(), name="account-admin-key"),
 ]
