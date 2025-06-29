0a0477d368b846fe5654acc66f027700b6d81f76
Salva05 <salvatorelombardi.98@hotmail.it>
2025-06-29 17:21:02 +0200

    refactor: remove unused import

--- backend/ioverse/apps/usage/urls.py
+++ backend/ioverse/apps/usage/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import OrgUsageView, OrgCostView
+from .views import OrgUsageView
 
 urlpatterns = [
     path("usage/", OrgUsageView.as_view(), name="org-usage"),
-    path("costs/", OrgCostView.as_view(), name="org-costs"),
 ]
