f4226c4ebf5dc6bd45522de202e1c2403851f92d
Salva05 <salvatorelombardi.98@hotmail.it>
2025-07-04 20:12:07 +0200

    style: positioning of imports

--- backend/ioverse/apps/account/views.py
+++ backend/ioverse/apps/account/views.py
@@ -3,7 +3,11 @@ from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .serializers import UserSerializer, UserRegistrationSerializer, AdminKeySetSerializer
+from .serializers import (
+    UserSerializer,
+    UserRegistrationSerializer,
+    AdminKeySetSerializer
+)
 from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib.auth import get_user_model
